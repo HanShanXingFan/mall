71b6754acd32f428d56fb8754052ff961f7f1cb1
shensheng <ss6813729@163.com>
2021-12-28 15:35:20 +0800

    路由封装完善

---
+++ src/pages/cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <order-header></order-header>
+      cart购物车
+      <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavFooter from '../components/NavFooter.vue'
+import OrderHeader from '../components/OrderHeader.vue'
+
+export default {
+  components: { NavFooter, OrderHeader },
+    name: 'Cart'
+}
+</script>
+
+<style>
+
+</style>
