a7d7b213fd830346448f0b3b18257717a9ed05fa
shensheng <ss6813729@163.com>
2021-12-28 15:51:25 +0800

    Revert "路由封装完善"
    This reverts commit 71b6754acd32f428d56fb8754052ff961f7f1cb1.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../pages/home.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,66 +8,16 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
-    redirect: '/index',
-    children: [
-      {
-        path: '/index',
-        name: 'index',
-        component: () => import(/* webpackChunkName: "index" */ '../pages/index.vue')
-      },
-      {
-        path: '/product/:id',
-        name: 'product',
-        component: () => import(/* webpackChunkName: "product" */ '../pages/product.vue')
-      },
-      {
-        path: '/detail/:id',
-        name: 'detail',
-        component: () => import(/* webpackChunkName: "detail" */ '../pages/detail.vue')
-      },
-    ]
+    component: Home
   },
   {
-    path: '/cart',
-    name: 'cart',
-    component: () => import(/* webpackChunkName: "cart" */ '../pages/cart.vue')
-  },
-  {
-    path: '/order',
-    name: 'order',
-    component: () => import(/* webpackChunkName: "order" */ '../pages/order.vue'),
-    children: [
-      {
-        path: '/list',
-        name: 'orderList',
-        component: () => import(/* webpackChunkName: "orderList" */ '../pages/orderList.vue'),
-      },
-      {
-        path: '/confirm',
-        name: 'orderConfirm',
-        component: () => import(/* webpackChunkName: "orderConfirm" */ '../pages/orderConfirm.vue'),
-      },
-      {
-        path: '/pay',
-        name: 'orderPay',
-        component: () => import(/* webpackChunkName: "orderPay" */ '../pages/orderPay.vue'),
-      },
-      {
-        path: '/alipay',
-        name: 'alipay',
-        component: () => import(/* webpackChunkName: "alipay" */ '../pages/aliPay.vue'),
-      }
-    ]
-  },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
